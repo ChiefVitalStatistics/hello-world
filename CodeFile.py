bb3a5281a005b785f146ae12a6cee46dc79902a9
ChiefVitalStatistics <unhygenix2003@yahoo.ie>
2019-06-12 19:12:07 +0100

    Readme edits atom (#3)
    * still in atom
    
    * Adding a monty file

---
+++ CodeFile.py
@@ -0,0 +1,10 @@
+# opening a python file and adding to the repository...
+
+import folium
+map = folium.Map(location=[53.863649,-9.307422], zoom_start=6) #, tiles="Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[53.863649,-9.307422], popup="Mustang Sally", icon=folium.Icon(color='green')))
+map.add_child(fg)
+
+map.save("Map1.html")
--- README.md
+++ README.md
@@ -5,4 +5,6 @@ I need to get a life, seriously.
 
 New TOol album coming soon.
 
-But what if I do this?
+ONce more with feeling
+
+added a python file...
